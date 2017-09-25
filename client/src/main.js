7b0678cda7bc97250b6e499d18c6c1b3ce8b82ff
mihaiconstantin <c.tinmihai@ymail.com>
2017-09-25 08:11:32 +0200

    Removed import for bootstrap.js

--- client/src/main.js
+++ client/src/main.js
@@ -3,10 +3,10 @@
 const jQuery = require('jquery');
 global.jQuery = jQuery;
 
-const Tether = require('Tether');
-global.Tether = Tether;
+// const Tether = require('Tether');
+// global.Tether = Tether;
 
-const Bootstrap = require('Bootstrap');
+// const Bootstrap = require('Bootstrap');
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Vue from 'vue'
